b98362f0e5a3d779f900a65b4daee92954cc4ba8
Junior Castillo <116048852+jicastilloh@users.noreply.github.com>
2023-08-08 20:01:31 -0600

    Se agregó la funcionalidad para crear una petición y validar usuarios en el login

--- client/src/landingPage/assets/js/controller.index.js
+++ client/src/landingPage/assets/js/controller.index.js
@@ -7,3 +7,36 @@ const signIn = document.getElementById("signIn")
 signIn.addEventListener("click", function(){
     window.location.href = "#signIn";
 })
+
+document.getElementById('btnInicio').addEventListener('click', async function() {
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const apiUrl = 'http://localhost:3000/login';
+
+    const formData = new URLSearchParams();
+    formData.append('username', username);
+    formData.append('password', password);
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: formData
+    };
+
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+        const data = await response.json();
+
+        if(data.length !== 0){
+            window.location.href = "../../renderer/views/infoUser.html"
+        }
+        console.log('Respuesta:', data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
+
+
